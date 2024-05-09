18efb7d0988283c9f428588d87f015e253327c69
N30A <95051018+N30A@users.noreply.github.com>
2024-05-09 23:37:25 +0200

    Update ComponentManager.java

--- src/utils/ComponentManager.java
+++ src/utils/ComponentManager.java
@@ -3,7 +3,6 @@ package utils;
 import javax.swing.DefaultListModel;
 import javax.swing.JList;
 import javax.swing.JTextField;
-import models.*;
 
 public class ComponentManager {
     
@@ -13,19 +12,12 @@ public class ComponentManager {
         }
     }
     
-    public static void updateListEntry(JList list, DefaultListModel<String> model, String newEntry) {
-        int selectedIndex = list.getSelectedIndex();
-        if (selectedIndex != -1) {
-            model.setElementAt(newEntry, selectedIndex);
-        } 
+    public static <T> void updateListEntry(JList list, DefaultListModel<T> model, T newEntry) {
+    int selectedIndex = list.getSelectedIndex();
+    if (selectedIndex != -1) {
+        model.setElementAt(newEntry, selectedIndex);
     }
-    
-    public static void updateListEntry(JList list, DefaultListModel<String> model, EmployeeModel newEntry) {
-        int selectedIndex = list.getSelectedIndex();
-        if (selectedIndex != -1) {
-            model.setElementAt(newEntry.toString(), selectedIndex);
-        } 
-    }
+}
     
     public static <T> void removeListEntry(JList list, DefaultListModel<T> model) {
         int selectedIndex = list.getSelectedIndex();
