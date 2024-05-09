5d0687d07533af609abaa3ecc4d0637baa43ee9a
dolken9 <gabriel.dolk@hotmail.com>
2024-05-09 22:29:03 +0200

    Påbörjat design goals, bajs

--- src/gui/goals/GoalsPanel.java
+++ src/gui/goals/GoalsPanel.java
@@ -14,11 +14,11 @@ public class GoalsPanel extends javax.swing.JPanel {
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 715, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 515, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
